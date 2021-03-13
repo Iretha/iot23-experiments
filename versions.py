4983067b879e001c1b1acb342cc9139365216922
M. Dimitrova <milena.bd@gmail.com>
2021-03-13 18:03:11 +0200

    Change deprecated method

---
+++ versions.py
@@ -0,0 +1,16 @@
+import sklearn
+import numpy
+import pandas
+import matplotlib
+import seaborn
+import psutil
+import scikitplot
+
+
+print('The scikit-learn version is {}.'.format(sklearn.__version__))
+print('The numpy version is {}.'.format(numpy.__version__))
+print('The pandas version is {}.'.format(pandas.__version__))
+print('The matplotlib version is {}.'.format(matplotlib.__version__))
+print('The seaborn version is {}.'.format(seaborn.__version__))
+print('The psutil version is {}.'.format(psutil.__version__))
+print('The scikitplot version is {}.'.format(scikitplot.__version__))
